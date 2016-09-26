3e31d06d7fdf0edeeeef96be28efb8944f1dfe6c
Liang Xingchen <liang@maichong.it>
2016-09-26 18:10:23 +0800

    0.2.3

---
+++ global.js
@@ -0,0 +1,19 @@
+/**
+ * @copyright Maichong Software Ltd. 2016 http://maichong.it
+ * @date 2016-09-26
+ * @author Liang <liang@maichong.it>
+ */
+
+'use strict';
+
+module.exports = {
+  Array: Array,
+  Date: Date,
+  Error: Error,
+  Function: Function,
+  Math: Math,
+  Object: Object,
+  RegExp: RegExp,
+  String: String,
+  TypeError: TypeError
+};
