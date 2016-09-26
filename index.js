b0b08394fd01ee81dba0b404f203b0de0555021a
Liang Xingchen <liang@maichong.it>
2016-09-26 15:21:29 +0800

    0.2.1

---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+ * @copyright Maichong Software Ltd. 2016 http://maichong.it
+ * @date 2016-09-26
+ * @author Liang <liang@maichong.it>
+ */
+
+'use strict';
+
+const labrador = module.exports = {};
+
+labrador.createPage = require('./create-page');
+
+const noPromise = {
+    some: true
+};
+
+function process(key) {
+    if (noPromise[key] || key.substr(0, 2) == 'on' || /\w+Sync$/.test(key)) {
+        if (wx.__lookupGetter__(key)) {
+            labrador.__defineGetter__(key, function () {
+                return wx[key];
+            });
+        } else {
+            labrador[key] = wx;
+        }
+        return;
+    }
+
+    labrador[key] = function (obj) {
+        obj = obj || {};
+        return new Promise(function (resolve, reject) {
+            obj.success = resolve;
+            obj.fail = function (res) {
+                if (res && res.errMsg) {
+                    reject(new Error(res.errMsg))
+                } else {
+                    reject(res);
+                }
+            };
+            wx[key](obj);
+        });
+    }
+}
+for (let key in wx) {
+    process(key);
+}
